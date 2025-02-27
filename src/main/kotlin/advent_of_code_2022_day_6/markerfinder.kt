54a681215ac890f9930633186d0cbb46fc3734b7
audioanton <antonlarsson.musik@gmail.com>
2025-02-27 21:26:07 +0100

    advent of code 2022 day 6 (part 2)

--- src/main/kotlin/advent_of_code_2022_day_6/markerfinder.kt
+++ src/main/kotlin/advent_of_code_2022_day_6/markerfinder.kt
@@ -3,27 +3,26 @@ package advent_of_code_2022_day_6
 import java.io.File
 
 fun main() {
-    val test1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb" // 7
-    val test2 = "bvwbjplbgvbhsrlpgdmjqwftvncz" // 5
-    val test3 = "nppdvjthqldpwncqszvftbrmjlhg" // 6
-    val test4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" // 10
-    val test5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" // 11
+    val test1 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb" // 7 - 19
+    val test2 = "bvwbjplbgvbhsrlpgdmjqwftvncz" // 5 - 23
+    val test3 = "nppdvjthqldpwncqszvftbrmjlhg" // 6 - 23
+    val test4 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" // 10 - 29
+    val test5 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" // 11 - 26
 
     val l = listOf(test1,test2,test3,test4,test5)
 
-    val data : List<String> = File("src/main/kotlin/aoc_1/test_input.txt").readLines()
+    val data : List<String> = File("src/main/kotlin/advent_of_code_2022_day_6/test_input.txt").readLines()
 
-//    l.forEach { println(findMarker(it)) }
+//    l.forEach { println(findMarker(it, 14)) }
 
-    println(findMarker(data[0]))
+    println(findMarker(data[0], 14))
 }
 
-
-fun findMarker(subroutine : String) : Int {
+fun findMarker(subroutine : String, routineLength : Int) : Int {
     fun findMarkerRecursive(start : Int, accumulator : Int, str : String) : Int {
-        return if (start+4 >= str.length ) -1
-        else if (str.subSequence(start, start+4).toSet().size == 4) accumulator+1
-        else findMarkerRecursive(start+1, start+4, str)
+        return if (start+routineLength >= str.length ) -1
+        else if (str.subSequence(start, start+routineLength).toSet().size == routineLength) accumulator+1
+        else findMarkerRecursive(start+1, start+routineLength, str)
     }
     return findMarkerRecursive(0, 0, subroutine)
 }
