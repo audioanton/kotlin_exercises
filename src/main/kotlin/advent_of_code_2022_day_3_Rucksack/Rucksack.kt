9ac4a5fdd8d431032045413c78c997f48d743d37
audioanton <antonlarsson.musik@gmail.com>
2025-03-06 15:49:44 +0100

    creating extension function and refactoring day 3 for more functional style

--- src/main/kotlin/advent_of_code_2022_day_3_Rucksack/Rucksack.kt
+++ src/main/kotlin/advent_of_code_2022_day_3_Rucksack/Rucksack.kt
@@ -8,26 +8,29 @@ fun main () {
     val letters = ('A'..'z').toList().filter { it.isLetter() }.partition { it.isUpperCase() }
     val letterScores = letters.first.mapIndexed { idx, c -> c to idx+27 }.toMap() + letters.second.mapIndexed { idx, c -> c to idx+1 }.toMap()
 
-    var sumPartOne = 0
+    val sumPartOne = lines.sumOf { getCompartments(it).getPriorityScore(letterScores) { l, c -> c in l[1] } }
 
-    for (line in lines) {
-        val compartmentOne = line.substring(0, line.length / 2).toSet().toString()
-        val compartmentTwo = line.substring(line.length / 2).toSet().joinToString("")
-        sumPartOne += getPriorityScore(listOf(compartmentOne, compartmentTwo), letterScores) { l, c  -> (c in l[1]) }
+    val sumPartTwoFunctional = (lines.indices step 3).sumOf { i -> listOf(lines[i], lines[i + 1], lines[i + 2]).map { it.toSet().joinToString("") }
+        .getPriorityScore(letterScores) { l, c -> (c in l[1] && c in l[2]) }
     }
-    println(sumPartOne)
 
+    // imperative, mutable..
     var sumPartTwo = 0
-
     for (i in lines.indices step 3) {
         val rucksacks = listOf(lines[i], lines[i+1], lines[i+2]).map { it.toSet().joinToString("") }
-        sumPartTwo += getPriorityScore(rucksacks, letterScores) { l, c -> (c in l[1] && c in l[2]) }
+        sumPartTwo += rucksacks.getPriorityScore(letterScores) { l, c -> (c in l[1] && c in l[2]) }
     }
+
+    println(sumPartOne)
     println(sumPartTwo)
+    println(sumPartTwoFunctional)
 }
 
-fun getPriorityScore(compartments : List<String>, scores : Map<Char, Int>, predicate : (List<String>, Char) -> Boolean ) : Int {
-    return compartments[0].filter { predicate(compartments, it) }.map { scores[it] ?: 0 }.sum()
+fun getCompartments(line : String) : List<String> = listOf(line.substring(0, line.length / 2).toSet().joinToString(""), line.substring(line.length / 2).toSet().joinToString(""))
+
+// extension function
+fun List<String>.getPriorityScore( scores : Map<Char, Int>, predicate : (List<String>, Char) -> Boolean ) : Int {
+    return this[0].filter { predicate(this, it) }.map { scores[it] ?: 0 }.sum()
 }
 
 
